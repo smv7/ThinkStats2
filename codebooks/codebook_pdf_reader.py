e4513e22e1a56b4adc7356a9f9a21ea7ee8d5271
Pascal Salvador <smv7.github@gmail.com>
2022-03-22 01:58:18 -0300

    Adding solutions to exercises of chapter 2

--- codebooks/codebook_pdf_reader.py
+++ codebooks/codebook_pdf_reader.py
@@ -9,17 +9,20 @@ import PyPDF2 as pypdf
 
 
 def main():
-    path = './Cycle6Codebook-Pregnancy.pdf'
+    path = input('File path: ')  #'./Cycle6Codebook-Pregnancy.pdf'
     pdf_file = pypdf.PdfFileReader(path)
     col_name = input('Column name: ').lower()
     #regex = f'.*{col_name}.+[0-9] of [0-9].*'
-
+    matches = []
     for i in range(pdf_file.numPages):
         current_page = pdf_file.getPage(i)
         text = current_page.extractText().lower()
         if col_name in text:#re.search(regex, text) is not None:
-            print(f'Page founded!\nYour result is in page {i+1}\n')
-            return i+1
+            matches.append(i+1)
+            
+    for p in matches:
+        print(f'Match in page {p}\n')
+
     return None
 
 
